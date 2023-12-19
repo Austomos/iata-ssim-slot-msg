7835028c7387a01647d2730267db6c0bde110f0f
Austomos <52321971+Austomos@users.noreply.github.com>
2023-12-19 17:05:47 +0100

    chore: set outline form for angular materiel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,16 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideClientHydration(),
 		provideAnimations(),
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: { appearance: 'outline' },
+		},
 	],
 };
