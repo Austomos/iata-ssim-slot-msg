81ccb738e33aabe049b69d37a064e495f2de14dc
Austomos <52321971+Austomos@users.noreply.github.com>
2023-12-21 14:22:11 +0100

    chore: remove angular material

--- src/app/helper/form-message/form-gcr-message/form-gcr-message.component.ts
+++ src/app/helper/form-message/form-gcr-message/form-gcr-message.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AirportInputComponent } from '../generic/airport-input/airport-input.component';
 
 @Component({
 	selector: 'app-form-gcr-message',
 	standalone: true,
-	imports: [],
+	imports: [AirportInputComponent],
 	templateUrl: './form-gcr-message.component.html',
 	styleUrl: './form-gcr-message.component.scss',
 })
--- src/app/helper/form-message/form-message.component.ts
+++ src/app/helper/form-message/form-message.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
 import { FormGcrMessageComponent } from './form-gcr-message/form-gcr-message.component';
 
 @Component({
 	selector: 'app-form-message',
 	standalone: true,
-	imports: [MatTabsModule, FormGcrMessageComponent],
+	imports: [FormGcrMessageComponent],
 	templateUrl: './form-message.component.html',
 	styleUrl: './form-message.component.scss',
 })
